e410a066dcd5e60bb58113417fb602ddc1cd71e8
Matheus Franco <matheusmfranco.mf@gmail.com>
2021-03-27 22:34:10 -0300

    Add files via upload

---
+++ trythencontinue.py
@@ -0,0 +1,41 @@
+#This is an example 
+#Code by Franco
+#27/03/2021
+# ------Lets Code!
+
+import time
+
+A = 1
+B = 2
+C = 3
+
+def showing():
+    print( A , B , C )
+    input()
+
+try:
+    showing()
+    print('Here you can put the code that you want to...')
+    #code
+    #code
+    #code
+    #code
+    #code
+    #code
+    #code
+    #code
+    #code
+    #code
+    #code
+    #code
+    #code
+    #code
+    #code
+    #code
+    #code
+    #code 
+    time.sleep(5)
+    
+except Exception as e:
+    print(e)
+    pass
